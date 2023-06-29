9ccac554cce9fb4363d5f919ff951b1ffdef0e3b
Alexander <Ra1jinThunderkeg@yandex.ru>
2023-06-29 18:13:53 +0300

    third step

---
+++ locales/ru.js
@@ -0,0 +1,13 @@
+export default {
+  ru: {
+    translation: {
+      validate: {
+        loadSuccess: 'Rss успешно загружен',
+        errors: {
+          urlRepeatable: 'RSS уже существует',
+          invalidUrl: 'Ссылка должна быть валидным URL',
+        },
+      },
+    },
+  },
+};
